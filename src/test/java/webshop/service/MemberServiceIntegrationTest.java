581040534b54e33e07157239d3c464dbf95c37fb
zcbm7008 <zcbm7008@gmail.com>
2024-01-29 17:18:28 +0900

    Refactor : MemberServiceTest into MemberServiceIntegrationTest and MemberServiceUnitTest

---
+++ src/test/java/webshop/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,68 @@
+package webshop.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import jakarta.transaction.Transactional;
+import webshop.User.domain.member.Member;
+import webshop.common.model.Money;
+import webshop.exception.NegativeBalanceException;
+import webshop.repository.MemberRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("/appConfig.xml")
+@Transactional
+public class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    public void Member_SignUp() throws Exception {
+
+        //Given
+        Member member = new Member("Kim");
+        //When
+        Long saveId = memberService.join(member);
+
+        //Then
+        Optional<Member> foundMember = memberRepository.findById(saveId);
+        assertTrue(foundMember.isPresent(), "Member not found");
+        assertEquals(member, foundMember.get());
+    }
+
+    @Test
+    public void Duplicated_member_exception() throws Exception {
+
+        //Given
+        Member member1 = new Member("Kim");
+
+        Member member2 = new Member("Kim");
+
+        //When
+        memberService.join(member1);
+
+        //Then
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        }, "Duplicate exception thrown");
+
+
+    }
+
+
+}
---
+++ src/test/java/webshop/service/MemberServiceUnitTest.java
@@ -0,0 +1,97 @@
+package webshop.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import webshop.User.domain.member.Member;
+import webshop.common.model.Money;
+import webshop.exception.NegativeBalanceException;
+import webshop.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceUnitTest {
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    MemberService memberService;
+
+    Member member1;
+
+    @BeforeEach
+    void Setup(){
+        member1 = new Member("Kim");
+
+        member1.setBalance(new Money(5000));
+
+        memberService.join(member1);
+    }
+
+
+    @Test
+    public void should_Increase_Member_Balance_When_Added() throws Exception {
+
+        //When
+        member1.addBalance(new Money(5000));
+        //Then
+        assertEquals(10000, member1.getBalance().getValue());
+
+    }
+
+    @Test
+    public void should_Decrease_Member_Balance_When_Subtracted() throws Exception {
+
+        //When
+        member1.subtractBalance(new Money(2000));
+
+        //Then
+        assertEquals(3000, member1.getBalance().getValue());
+    }
+
+    @Test
+    public void Negative_Balance_Exception() throws Exception {
+
+        //When
+        assertThrows(NegativeBalanceException.class, () -> {
+            member1.subtractBalance(new Money(9000));
+        }, "NegativeBalanceException thrown");
+    }
+
+    @Test
+    public void Member_Balance_Added_or_Subtracted_Zero() throws Exception {
+
+        //When
+        member1.subtractBalance(new Money(0));
+
+        //Then
+        assertEquals(5000, member1.getBalance().getValue());
+
+        //When
+        member1.addBalance(new Money(0));
+
+        //Then
+        assertEquals(5000, member1.getBalance().getValue());
+    }
+
+    @Test
+    public void Block_Member() throws Exception{
+
+        //When
+        member1.block();
+
+        //Then
+        assertEquals(member1.isBlocked(),true);
+
+        //When
+        member1.unblock();
+
+        //Then
+        assertEquals(member1.isBlocked(),false);
+    }
+}
