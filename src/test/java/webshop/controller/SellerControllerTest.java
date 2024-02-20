71ee198e88141602e16257d0edbcee4b995e35a4
zcbm7008 <zcbm7008@gmail.com>
2024-02-20 17:00:26 +0900

    test : test for new item creation by seller
    This commit introduces a unit test for the feature where sellers can create new items. The test verifies the integration of the `SellerService` and `ItemService` with the controller, ensuring that a POST request to the `/my/sellers/{sellerId}/items/new` endpoint correctly saves the new artwork with specified attributes. It checks for the expected redirection status and validates the properties of the saved artwork object.

--- src/test/java/webshop/controller/SellerControllerTest.java
+++ src/test/java/webshop/controller/SellerControllerTest.java
@@ -1,17 +1,33 @@
 package webshop.controller;
 
-import jakarta.transaction.Transactional;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import webshop.catalog.command.domain.product.Artwork;
+import webshop.catalog.command.domain.product.Item;
+import webshop.catalog.query.product.ItemService;
+import webshop.user.domain.seller.Seller;
+import webshop.user.domain.seller.SellerService;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
@@ -21,6 +37,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SellerControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private SellerService sellerService;
+    @MockBean
+    private ItemService itemService;
+    @Mock
+    Seller seller;
+
+    @Captor
+    private ArgumentCaptor<Item> artworkCaptor;
 
     @Test
     @WithMockUser
@@ -37,5 +62,29 @@ public class SellerControllerTest {
         mockMvc.perform(get("/my/sellers/new"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("member/createSellerForm"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testSellerCreateNewItem() throws Exception {
+
+        given(sellerService.findById(anyLong())).willReturn(seller);
+
+        mockMvc.perform(post("/my/sellers/{sellerId}/items/new",seller.getId())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "Artwork Name")
+                .param("price", "100")
+                .param("content", "Artwork Description")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection()); // 예상되는 리디렉션 또는 다른 상태 코드
+
+        verify(itemService).saveItem(artworkCaptor.capture());
+        Artwork capturedArtwork = (Artwork) artworkCaptor.getValue();
+
+        // 캡처된 Artwork 객체의 속성 검증
+        assertEquals("Artwork Name", capturedArtwork.getName());
+        assertEquals(100, capturedArtwork.getPrice().getValue());
+        assertEquals("Artwork Description", capturedArtwork.getContent());
+
     }
 }
